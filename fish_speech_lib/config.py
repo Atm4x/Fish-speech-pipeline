5fb8bce220cbc77e81de658843f88e5698e2ae3a
artem hakaev <mrtaktplay@gmail.com>
2025-03-02 18:42:52 +0300

    test 6

--- fish_speech_lib/config.py
+++ fish_speech_lib/config.py
@@ -1,7 +1,7 @@
 FIREFLY_GAN_VQ_CONFIG = {
-    "_target_": "fish_speech.models.vqgan.modules.firefly.FireflyArchitecture",
+    "_target_": "fish_speech_lib.fish_speech.models.vqgan.modules.firefly.FireflyArchitecture",
     "spec_transform": {
-        "_target_": "fish_speech.utils.spectrogram.LogMelSpectrogram",
+        "_target_": "fish_speech_lib.fish_speech.utils.spectrogram.LogMelSpectrogram",
         "sample_rate": 44100,
         "n_mels": 160,
         "n_fft": 2048,
@@ -9,7 +9,7 @@ FIREFLY_GAN_VQ_CONFIG = {
         "win_length": 2048,
     },
     "backbone": {
-        "_target_": "fish_speech.models.vqgan.modules.firefly.ConvNeXtEncoder",
+        "_target_": "fish_speech_lib.fish_speech.models.vqgan.modules.firefly.ConvNeXtEncoder",
         "input_channels": 160,
         "depths": [3, 3, 9, 3],
         "dims": [128, 256, 384, 512],
@@ -17,7 +17,7 @@ FIREFLY_GAN_VQ_CONFIG = {
         "kernel_size": 7,
     },
     "head": {
-        "_target_": "fish_speech.models.vqgan.modules.firefly.HiFiGANGenerator",
+        "_target_": "fish_speech_lib.fish_speech.models.vqgan.modules.firefly.HiFiGANGenerator",
         "hop_length": 512,
         "upsample_rates": [8, 8, 2, 2, 2],  # aka. strides
         "upsample_kernel_sizes": [16, 16, 4, 4, 4],
@@ -29,7 +29,7 @@ FIREFLY_GAN_VQ_CONFIG = {
         "post_conv_kernel_size": 13,
     },
     "quantizer": {
-        "_target_": "fish_speech.models.vqgan.modules.fsq.DownsampleFiniteScalarQuantize",
+        "_target_": "fish_speech_lib.fish_speech.models.vqgan.modules.fsq.DownsampleFiniteScalarQuantize",
         "input_dim": 512,
         "n_groups": 8,
         "n_codebooks": 1,
